cf4b4c74e1cd2ed632e793a1b47b7351f6f1a647
yusuf <yusuf@HP-0106072002>
2024-03-26 21:09:52 +0300

    Add project files.

---
+++ Sequential Search C#/Program.cs
@@ -0,0 +1,20 @@
+﻿Search();
+
+static void Search()
+{
+    Random random = new Random();
+    int[] array = Enumerable.Repeat(0, 10).Select(i => random.Next(0, 20)).ToArray();
+    int searchElement = random.Next(0, 30);
+
+    Console.WriteLine($"Initial Array:{string.Join(", ", array)}");
+    Console.WriteLine($"Element to be searched in the array: {searchElement}");
+
+    bool found = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == searchElement) { found = true; break; }
+    }
+
+    Console.WriteLine($"Element was {(found ? "found" : "not found")} in the array.");
+    Console.ReadKey();
+}
